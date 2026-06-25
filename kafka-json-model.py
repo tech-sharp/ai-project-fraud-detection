a8e371d18cafe74bdacf094a36f1fe209bf6946d
tech-sharp <cool.tech22@outlook.com>
2026-06-25 01:01:49 -0300

    add content kafka-json-model.py

--- kafka-json-model.py
+++ kafka-json-model.py
@@ -46,4 +46,4 @@ if __name__ == "__main__":
         format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
         level=logging.INFO
         )
-    main()
+    main()
